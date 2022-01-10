4d80eb412bdb2fc23e052c364d0c93c3cc7d078f
GuidoAsbun <guido@asbun.io>
2022-01-09 16:15:01 -0800

    Created student model for db with constraints

--- src/main/java/io/asbun/jdademo/Student.java
+++ src/main/java/io/asbun/jdademo/Student.java
@@ -1,11 +1,32 @@
 package io.asbun.jdademo;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Student") // maps class to db
+@Table(name = "student", uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+})
 public class Student {
 
+    @Id // sets id as primary key
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = SEQUENCE, generator = "student_sequence")
+    @Column(name = "id", updatable = false)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
     private String lastName;
+
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
     private String email;
+
+    @Column(name = "age", nullable = false)
     private Integer age;
 
     public Student(Long id, String firstName, String lastName, String email, Integer age) {
@@ -14,6 +35,10 @@ public class Student {
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
